b34b7ed8547b5bd75a0183146460d2dfd0f61788
Fabian Bertrand <fabianbertrand94@gmail.com>
2022-10-18 21:43:25 -0400

    Edited tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -14,13 +14,14 @@ document.addEventListener('DOMContentLoaded', function () {
             boardDivs[i].className = "square";
     
     }
-
+    
     let currentPlayer = "X";
 
     let square = document.getElementsByClassName("square");
 
     var played = new Array(9);
     
+    let status = document.getElementById("status");
     
     for (let i = 0; i < square.length; i++) {
 
@@ -34,6 +35,13 @@ document.addEventListener('DOMContentLoaded', function () {
                     square[i].classList.add("X");
                     played[i] = "X";
                     square[i].innerHTML = "X";
+
+                   if (checkrow(square) || checkcol(square) || checkdiag(square)) {
+    
+                        status.innerHTML = `Congratulations! ${currentPlayer} is the Winner`;
+                        status.className = "you-won";
+                    }
+
                     currentPlayer = "O";    
 
                 } else {
@@ -41,11 +49,17 @@ document.addEventListener('DOMContentLoaded', function () {
                     square[i].classList.add("O");
                     played[i] = "O";
                     square[i].innerHTML = "O";
+                    if (checkrow(square) || checkcol(square) || checkdiag(square)) { 
+           
+                        status.innerHTML = `Congratulations! ${currentPlayer} is the Winner`;
+                        status.className = "you-won";
+                    }
+
                     currentPlayer = "X"
                 }
             }
         });
-
+        
         // Add hover class if mouse is over the square
         square[i].addEventListener("mouseover", function() {
             square[i].classList.add("hover");
@@ -57,3 +71,68 @@ document.addEventListener('DOMContentLoaded', function () {
         })
     }
 });
+
+// Checks rowss for match
+function checkrow (square) {
+
+        if  (square[0].innerHTML != "") {
+
+             if (square[0].innerHTML == square[1].innerHTML && square[0].innerHTML == square[2].innerHTML) {
+                return true;
+             }
+        }
+
+        if (square[3].innerHTML != "") {
+
+            if (square[3].innerHTML == square[4].innerHTML && square[3].innerHTML == square[5].innerHTML) {
+                return true;
+            } 
+          
+        }
+
+        if (square[6].innerHTML != "") {
+
+           if (square[6].innerHTML == square[7].innerHTML && square[6].innerHTML == square[8].innerHTML){
+            return true;
+           }
+        }
+}
+
+// Checks columns for match
+function checkcol(square) {
+    
+    if  (square[0].innerHTML != "") {
+
+        if (square[0].innerHTML == square[3].innerHTML && square[0].innerHTML == square[6].innerHTML) {
+           return true;
+        }
+   }
+
+   if (square[1].innerHTML != "") {
+
+       if (square[1].innerHTML == square[4].innerHTML && square[1].innerHTML == square[7].innerHTML) {
+           return true;
+       } 
+     
+   }
+
+   if (square[2].innerHTML != "") {
+
+      if (square[2].innerHTML == square[5].innerHTML && square[2].innerHTML == square[8].innerHTML){
+       return true;
+      }
+   }
+    
+}
+
+// Checks diagonals for match
+function checkdiag(square) {
+
+    if (square[4].innerHTML != "") {
+
+        if ((square[0].innerHTML == square[4].innerHTML && square[0].innerHTML == square[8].innerHTML) || (square[2].innerHTML == square[4].innerHTML && square[2].innerHTML == square[6].innerHTML)) {
+            return true;
+        }
+    }
+}
+    
